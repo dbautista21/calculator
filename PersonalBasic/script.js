aa033c0a4ffb37beb3438c85f17af1f11381cdeb
Dilan Bautista <dilan_bautista@ymail.com>
2022-11-29 09:56:17 -0500

    Adding original files

---
+++ PersonalBasic/script.js
@@ -0,0 +1,52 @@
+const elements = [
+  "7",
+  "8",
+  "9",
+  "-",
+  "CE",
+  "4",
+  "5",
+  "6",
+  "+",
+  "X",
+  "1",
+  "2",
+  "3",
+  "/",
+  "=",
+];
+const parent = document.getElementById("calc");
+const numInput = function (x) {
+  console.log(x);
+};
+const subtract = function () {};
+const plus = function () {};
+const divide = function () {};
+const equal = function () {};
+const clear = function () {};
+const multiply = function () {};
+
+/* 
+switch (elements[i]) {
+  case "CE":
+    button.addEventListener("click", clear());
+    break;
+  case "=":
+    button.addEventListener("click", equal());
+    break;
+  case "+":
+    button.addEventListener("click", plus());
+    break;
+  case "-":
+    button.addEventListener("click", subtract());
+    break;
+  case "X":
+    button.addEventListener("click", multiply());
+    break;
+  case "/":
+    button.addEventListener("click", divide());
+    break;
+  default:
+    button.addEventListener("click", numInput(button.innerHTML));
+}
+ */

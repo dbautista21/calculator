58d1e51b9bd259c21e53b59394ecf001ffeda726
Dilan Bautista <dilan_bautista@ymail.com>
2022-11-29 11:14:21 -0500

    Calc works basically

--- PersonalBasic/script.js
+++ PersonalBasic/script.js
@@ -1,52 +1,34 @@
-const elements = [
-  "7",
-  "8",
-  "9",
-  "-",
-  "CE",
-  "4",
-  "5",
-  "6",
-  "+",
-  "X",
-  "1",
-  "2",
-  "3",
-  "/",
-  "=",
-];
-const parent = document.getElementById("calc");
-const numInput = function (x) {
-  console.log(x);
-};
-const subtract = function () {};
-const plus = function () {};
-const divide = function () {};
-const equal = function () {};
-const clear = function () {};
-const multiply = function () {};
+const buttons = document.querySelectorAll(".btn-op");
+const display = document.getElementById("display");
 
-/* 
-switch (elements[i]) {
-  case "CE":
-    button.addEventListener("click", clear());
-    break;
-  case "=":
-    button.addEventListener("click", equal());
-    break;
-  case "+":
-    button.addEventListener("click", plus());
-    break;
-  case "-":
-    button.addEventListener("click", subtract());
-    break;
-  case "X":
-    button.addEventListener("click", multiply());
-    break;
-  case "/":
-    button.addEventListener("click", divide());
-    break;
-  default:
-    button.addEventListener("click", numInput(button.innerHTML));
+const numInput = function () {
+  display.innerHTML += this.value;
+};
+const subtract = function () {
+  display.innerHTML += "-";
+};
+const plus = function () {
+  display.innerHTML += "+";
+};
+const divide = function () {
+  display.innerHTML += "/";
+};
+const equal = function () {
+  display.innerHTML = eval(display.innerHTML);
+};
+const clear = function () {
+  display.innerHTML = "";
+};
+const multiply = function () {
+  display.innerHTML += "*";
+};
+
+for (let i = 0; i < buttons.length; i++) {
+  if (buttons[i].value == "CE") {
+    buttons[i].addEventListener("click", clear);
+  } else if (buttons[i].value == "=") {
+    buttons[i].addEventListener("click", equal);
+  } else {
+    buttons[i].addEventListener("click", numInput);
+  }
 }
- */

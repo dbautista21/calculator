56f9bfd4ca092904316e48a1bd2fa9e2dda70c9b
Dilan Bautista <dilan_bautista@ymail.com>
2022-11-29 13:52:15 -0500

    Added feature for starting an operation

--- PersonalBasic/script.js
+++ PersonalBasic/script.js
@@ -1,34 +1,70 @@
+"use script";
+
 const buttons = document.querySelectorAll(".btn-op");
 const display = document.getElementById("display");
+display.innerText = "0";
+let op_limit = false;
 
-const numInput = function () {
+const isOp = function (x) {
+  if (x === "+" || x === "-" || x === "/" || x === "*") {
+    return true;
+  }
+  return false;
+};
+//display.innerHTML = "9+88*";
+const input = function () {
+  last_char = display.innerHTML.charAt(display.innerHTML.length - 1);
+  // if the last character is a zero and the current one is not an operation
+  // Just replace the zero with the current input
+  if (
+    last_char === "0" &&
+    !isOp(this.value) &&
+    display.innerHTML.length === 1
+  ) {
+    display.innerHTML = this.value;
+    return;
+  }
+  // if the last character is an operation and the current one is not an operation
+  // reached maximum number of operations: next operation will result in eval
+  if (isOp(last_char) && !isOp(this.value)) {
+    op_limit = true;
+    display.innerHTML += this.value;
+    return;
+  }
+  // if the last character is an operation and the current one is an operation
+  if (isOp(last_char) && isOp(this.value)) {
+    if (last_char == this.value) {
+      return;
+    } else {
+      display.innerHTML =
+        display.innerHTML.substring(0, display.innerHTML.length - 1) +
+        this.value;
+      return;
+    }
+  }
+
+  // if the last character is a number other than
   display.innerHTML += this.value;
-};
-const subtract = function () {
-  display.innerHTML += "-";
-};
-const plus = function () {
-  display.innerHTML += "+";
-};
-const divide = function () {
-  display.innerHTML += "/";
 };
 const equal = function () {
   display.innerHTML = eval(display.innerHTML);
 };
 const clear = function () {
-  display.innerHTML = "";
-};
-const multiply = function () {
-  display.innerHTML += "*";
+  display.innerHTML = "0";
 };
 
+//Add the event listeners to each button : clear/equal/input
 for (let i = 0; i < buttons.length; i++) {
   if (buttons[i].value == "CE") {
     buttons[i].addEventListener("click", clear);
   } else if (buttons[i].value == "=") {
     buttons[i].addEventListener("click", equal);
   } else {
-    buttons[i].addEventListener("click", numInput);
+    buttons[i].addEventListener("click", input);
   }
 }
+// Goal: Save calculation
+// Objective: When pressing an operation key multiple times allow the last key to change
+// on the display line
+
+// Add a variable counter
--- README.md
+++ README.md
@@ -11,3 +11,14 @@ Design Flaw: Probably will start a new branch about this. I'm trying to have a b
 to be sitting on the top of the calculator. 
 Other than that, I'm proud to say the calculator is fully functional. 
 Next step after fixing bugs and adding a nice title, would be to allow the page to be resizable and mobile device adaptive. 
+
+Objectives: I guess the next thing I could do is create a variable to hold a value of
+true or false if a certain key is pressed. 
+For example if 3 is pressed, then x. The variable would turn true. Now there has to be a
+fork here. If a different operation key is pressed.
+
+11/29/2022
+
+Completed a small side thing that is an importan part of the calculator.I integrated how the 
+calculator should function if the system will start with a number zero in the beginning. 
+
